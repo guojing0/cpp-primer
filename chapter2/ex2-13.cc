928b3ddef3612ec7be6667cce8541520d44fa3fc
guoj <dev.guoj@gmail.com>
2015-03-10 19:46:29 +0800

    some of chapter two

---
+++ chapter2/ex2-13.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int i = 42;
+
+int main(int argc, char const *argv[])
+{
+    int i = 100, j = i;
+    std::cout << j << std::endl;
+
+    return 0;
+}
---
+++ chapter2/ex2-8.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "2M" << std::endl;
+    std::cout << 2 << "\v" << "M" << std::endl; 
+
+    return 0;
+}
